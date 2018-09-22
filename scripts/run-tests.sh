2b4ee0c9dd26f4f3348cbd144ab632e88c8eab59
haoyuan ge <haoyuan.ge@outlook.com>
2018-09-22 18:16:50 +0800

    delay tests

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+sleep 5
+docker ps
 curl localhost:8001/health/live
-curl localhost:8001/airports/NL
 curl localhost:8002/health/live
+curl localhost:8001/airports/NL
 curl localhost:8002/countries/Netherlands
