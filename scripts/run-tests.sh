e43a08b9cf98fc8977690ca5d59f5d81b7a7f9f5
haoyuan ge <haoyuan.ge@outlook.com>
2018-09-22 19:22:44 +0800

    Change testing scripts

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-sleep 5
-docker ps
-curl localhost:8001/health/live
-curl localhost:8002/health/live
+echo "Testing airports-assembly"
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8001/health/ready)" != "200" ]]; do 
+	echo "Airports service still initializing...Wait 5s"
+	sleep 5; 
+done
 curl localhost:8001/airports/NL
+
+echo "Testing countries-assembly"
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8002/health/ready)" != "200" ]]; do
+	echo "Countries service still initializing...Wait 5s"
+ 	sleep 5;
+done
 curl localhost:8002/countries/Netherlands
